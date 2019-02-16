03ebeec688183281d5ce30b1a1509fa320a174db
kwesidev <kwesidev@gmail.com>
2019-02-16 19:49:35 +0200

    format

--- src/main/java/tk/kwesidev/jpatutorials/DBManager.java
+++ src/main/java/tk/kwesidev/jpatutorials/DBManager.java
@@ -1,27 +1,26 @@
 package tk.kwesidev.jpatutorials;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.EntityManager;
+
 public class DBManager {
-	
+
 	private EntityManagerFactory emfactory;
 
-    
 	public DBManager() {
 
 		emfactory = Persistence.createEntityManagerFactory("jpatest");
-		
+
 	}
+
 	/**
 	 * @return EntityManager
 	 */
 	public EntityManager getEntityManager() {
-		
+
 		return emfactory.createEntityManager();
-		
-		
+
 	}
-	
-	
+
 }
